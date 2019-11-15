54af467e87792448bf7513d34d3657b09e18eede
Clément Lesné <clement@lesne.pro>
2019-11-15 23:11:31 +0100

    Test: Change test coverage compliance to 80%

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,9 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 90,
-      functions: 90,
-      lines: 90,
+      statements: 80,
+      functions: 80,
+      lines: 80,
     },
   },
   moduleDirectories: ['node_modules', 'app'],
